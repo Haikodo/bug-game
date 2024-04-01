e4a51888caa594356a707b41cdb9f09260b58dc4
Haikodo <aaronls2404@gmail.com>
2024-04-01 18:51:45 -0500

    Save, Load, Reset
    FINALLY FUKCING MADE IST ASGHAGH

--- v1/game.js
+++ v1/game.js
@@ -1,13 +1,3 @@
-onload => localStorage.getItem("player");
-
-function load() {
-  JSON.parse(localStorage.getItem("player"));
-}
-
-function restart() {
-  localStorage.removeItem("player");
-}
-
 let biomassCount = 0;
 let biomassPerSecond = 0;
 const baseUpgradeCost = 10;
@@ -17,34 +7,55 @@ let newUpgradeCost = 10;
 
 function updateDisplay() {
   document.getElementById("biomassCount").textContent = parseInt(biomassCount);
-  document.getElementById("biomassPerSecond").textContent =
-    parseInt(biomassPerSecond);
+  document.getElementById("biomassPerSecond").textContent =parseInt(biomassPerSecond);
   document.getElementById("upgradeCost").textContent = parseInt(newUpgradeCost);
-}
+  newUpgradeCost = baseUpgradeCost * 1.05 ** passiveBiomassCollectors
+};
 
 function clickBiomass() {
   biomassCount++;
   updateDisplay();
-}
+};
 
 function buyUpgrade() {
   if (biomassCount >= newUpgradeCost) {
     biomassCount -= newUpgradeCost;
     biomassPerSecond++;
     passiveBiomassCollectors++;
-    newUpgradeCost = baseUpgradeCost * 1.05 ** passiveBiomassCollectors;
     updateDisplay();
   }
-}
+};
+
+function load() {
+  var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+  if (typeof savedGame.biomassCount !== "undefined") biomassCount = savedGame.biomassCount;
+  if (typeof savedGame.biomassPerSecond !== "undefined") biomassPerSecond = savedGame.biomassPerSecond;
+  if (typeof savedGame.passiveBiomassCollectors !== "undefined") passiveBiomassCollectors = savedGame.passiveBiomassCollectors;
+  if (typeof savedGame.passiveBiomassCollection !== "undefined") passiveBiomassCollection = savedGame.passiveBiomassCollection;
+};
 
 function save() {
-  var player = {
+  var gameSave = {
     biomassCount: biomassCount,
     biomassPerSecond: biomassPerSecond,
     passiveBiomassCollectors: passiveBiomassCollectors,
-    passiveBiomassCollection: passiveBiomassCollection,
+    passiveBiomassCollection: passiveBiomassCollection
   };
-  localStorage.setItem("player", JSON.stringify(player));
+  localStorage.setItem("gameSave", JSON.stringify(gameSave));
+};
+
+function reset() {
+  localStorage.removeItem("gameSave");
+  location.reload();
+  updateDisplay();
+};
+
+window.onload = function() {
+  load();
+  updateDisplay();
+  document.getElementById("biomassCount").textContent = parseInt(biomassCount);
+  document.getElementById("biomassPerSecond").textContent = parseInt(biomassPerSecond);
+  document.getElementById("upgradeCost").textContent = parseInt(newUpgradeCost);
 }
 
 setInterval(() => {
